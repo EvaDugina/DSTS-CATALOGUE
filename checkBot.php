4fe16d51a5c4e203896e131327e6c977645ebf3b
EvaDugina <vania.dugin@bk.ru>
2025-02-12 21:45:32 +0300

    checkBot

--- checkBot.php
+++ checkBot.php
@@ -10,18 +10,35 @@ show_head("СТРАНИЦА ИНФОРМАЦИИ О ТОВАРЕ");
 ?>
 
 <body style="overflow-x: hidden;">
-    <main class="d-flex flex-column">
+    <main class="d-flex flex-column p-5">
 
         <div class="d-flex mb-3">
-            <!-- <button class="btn btn-success me-3" onclick="startDaemon()">START DAEMON</button>
-            <button class="btn btn-danger me-3" onclick="stopDaemon()">STOP DAEMON</button> -->
-            <button class="btn btn-primary" onclick="functions.checkConnectionToServer()">CHECK CONNECTION!</button>
+            <button class="btn btn-success me-3" onclick="functions.checkConnectionToServer()">CHECK CONNECTION</button>
+            <button class="btn btn-warning me-3" onclick="functions.updateLogProgressResult()">GET LOG, PROGRESS, RESULT</button>
+            <button class="btn btn-danger" onclick="functions.sendStopSearchRequest()">STOP SEARCHING</button>
         </div>
 
-        <div class="d-flex">
-            <button class="btn btn-success me-3" onclick="functions.sendSearchRequest()">START SEARCHING</button>
-            <button class="btn btn-danger me-3" onclick="functions.sendStopSearchRequest()">STOP SEARCHING</button>
-            <button class="btn btn-primary" onclick="functions.sendGetSearchProgressRequest()">GET SEARCH PROGRESS</button>
+        <div class="d-flex mb-3">
+            <button class="btn btn-primary me-3" onclick="functions.sendDonaldsonP55077SearchRequest()">DONALDSON P55077</button>
+            <button class="btn btn-primary me-3" onclick="functions.sendFleetguardP55077SearchRequest()">FLEETGUARD P55077</button>
+            <button class="btn btn-primary me-3" onclick="functions.sendMannP55077SearchRequest()">MANN P55077</button>
+            <button class="btn btn-primary me-3" onclick="functions.sendHifiSh60161SearchRequest()">HIFI SH60161</button>
+            <button class="btn btn-primary me-3" onclick="functions.sendFilfilterP55077SearchRequest()">FILFILTER P55077</button>
+        </div>
+
+        <div class="d-flex mb-3">
+            <button class="btn btn-secondary me-3" onclick="functions.sendFastSearchRequest()">Быстрая проверка</button>
+            <button class="btn btn-secondary me-3" onclick="functions.sendStandartSearchRequest()">Стандартная проверка</button>
+            <button class="btn btn-secondary me-3" onclick="functions.sendDeepSearchRequest()">Глубокая проверка</button>
+        </div>
+
+        <div class="d-flex mb-3" style="height: 1000px;">
+            <textarea id="textarea-log" class="w-100 h-100" style="overflow-y: scroll;" readonly></textarea>
+        </div>
+
+        <div class="d-flex" style="height: 500px;">
+            <textarea id="textarea-progress" class="w-100 h-100 me-3" style="overflow-y: scroll;" readonly></textarea>
+            <textarea id="textarea-result" class="w-75 h-100" style="overflow-y: scroll;" readonly></textarea>
         </div>
 
     </main>
@@ -35,31 +52,169 @@ show_head("СТРАНИЦА ИНФОРМАЦИИ О ТОВАРЕ");
     import ServerHandler from "./js/ServerHandler.js";
 
     var serverHandler = new ServerHandler();
+    var FLAG_END = true;
+
+    // 
+    // 
+    // 
 
     export async function checkConnectionToServer() {
-        new ServerHandler();
+        serverHandler = new ServerHandler();
     }
     functions.checkConnectionToServer = checkConnectionToServer;
 
+    // 
+    // 
+    // 
 
-    export async function sendSearchRequest() {
-        sendRequest(serverHandler.getTestSearchRequestData());
-    }
-    functions.sendSearchRequest = sendSearchRequest;
+    export async function checkSearchLogProgressResult() {
+        if (FLAG_END == true)
+            return;
 
-    export async function sendGetSearchProgressRequest() {
-        sendRequest(serverHandler.getSearchProgressRequestData());
+        await checkSearchFlagEndRequest();
+        await updateLogProgressResult();
     }
-    functions.sendGetSearchProgressRequest = sendGetSearchProgressRequest;
+    functions.checkSearchLogProgressResult = checkSearchLogProgressResult;
+    setInterval(checkSearchLogProgressResult, 5000);
+
+    export async function checkSearchFlagEndRequest() {
+        await sendRequest(serverHandler.getGetSearchFlagEndRequestData(), true, function(server_data) {
+            FLAG_END = server_data['flag_end'];
+        });
+        if (FLAG_END == true)
+            alert("Поиск окончен!");
+    }
+    functions.checkSearchFlagEndRequest = checkSearchFlagEndRequest;
 
     export async function sendStopSearchRequest() {
         sendRequest(serverHandler.getStopSearchRequestData());
+        FLAG_END = true;
     }
     functions.sendStopSearchRequest = sendStopSearchRequest;
 
-    async function sendRequest(sendingData) {
+
+    // 
+    // 
+    // 
+
+    export async function updateLogProgressResult() {
+        await sendRequest(serverHandler.getGetLogProgressResultRequestData(), true, function(server_data) {
+            document.getElementById('textarea-log').value = server_data['logs'].join("\n");
+            document.getElementById('textarea-log').scrollTop = textarea.scrollHeight;
+
+            document.getElementById('textarea-progress').value = server_data['progress'].join("\n");
+
+            let text = "";
+            server_data['result'].forEach((element) => {
+                for (let [key, value] of Object.entries(element)) {
+                    text += value + "\t|\t";
+                }
+                text += "\n"
+            });
+            document.getElementById('textarea-result').value = text;
+        });
+    }
+    functions.updateLogProgressResult = updateLogProgressResult;
+
+    export async function updateLog() {
+        await sendRequest(serverHandler.getGetLogRequestData(), true, function(server_data) {
+            document.getElementById('textarea-log').value = server_data['logs'].join("\n")
+        });
+    }
+    functions.updateLog = updateLog;
+
+    export async function updateProgress() {
+        await sendRequest(serverHandler.getGetProgressRequestData(), true, function(server_data) {
+            document.getElementById('textarea-progress').value = server_data['progress'].join("\n");
+        });
+    }
+    functions.updateProgress = updateProgress;
+
+    export async function updateResult() {
+        await sendRequest(serverHandler.getGetResultRequestData(), true, function(server_data) {
+            let text = "";
+            server_data['result'].forEach((element) => {
+                for (let [key, value] of Object.entries(element)) {
+                    text += value + "\t|\t";
+                }
+                text += "\n"
+            });
+            document.getElementById('textarea-result').value = text;
+        });
+    }
+    functions.updateResult = updateResult;
+
+    // 
+    // 
+    // 
+
+    export async function sendSearchRequest(search_request) {
+        sendRequest(search_request);
+        FLAG_END = false;
+    }
+    functions.sendSearchRequest = sendSearchRequest;
+
+
+
+    export async function sendDonaldsonP55077SearchRequest() {
+        sendSearchRequest(serverHandler.getDonaldsonP55077SearchRequestData())
+    }
+    functions.sendDonaldsonP55077SearchRequest = sendDonaldsonP55077SearchRequest;
+
+    export async function sendFleetguardP55077SearchRequest() {
+        sendSearchRequest(serverHandler.getFleetguardP55077SearchRequestData())
+    }
+    functions.sendFleetguardP55077SearchRequest = sendFleetguardP55077SearchRequest;
+
+    export async function sendMannP55077SearchRequest() {
+        sendSearchRequest(serverHandler.getMannP55077SearchRequestData())
+    }
+    functions.sendMannP55077SearchRequest = sendMannP55077SearchRequest;
+
+    export async function sendHifiSh60161SearchRequest() {
+        sendSearchRequest(serverHandler.getHifiSh60161SearchRequestData())
+    }
+    functions.sendHifiSh60161SearchRequest = sendHifiSh60161SearchRequest;
+
+    export async function sendFilfilterP55077SearchRequest() {
+        sendSearchRequest(serverHandler.getFilfilterP55077SearchRequestData())
+    }
+    functions.sendFilfilterP55077SearchRequest = sendFilfilterP55077SearchRequest;
+
+    // 
+    // 
+    // 
+
+    export async function sendFastSearchRequest() {
+        sendSearchRequest(serverHandler.getFastCheckSearchRequestData())
+    }
+    functions.sendFastSearchRequest = sendFastSearchRequest;
+
+    export async function sendStandartSearchRequest() {
+        sendSearchRequest(serverHandler.getStandartCheckSearchRequestData())
+    }
+    functions.sendStandartSearchRequest = sendStandartSearchRequest;
+
+    export async function sendDeepSearchRequest() {
+        sendSearchRequest(serverHandler.getDeepCheckTestSearchRequestData())
+    }
+    functions.sendDeepSearchRequest = sendDeepSearchRequest;
+
+    // 
+    // 
+    // 
+
+    async function sendRequest(sendingData, flag_wait_for_answer = false, callback = null) {
+        if (!serverHandler.isEnabled()) {
+            checkConnectionToServer();
+        }
+
         console.log(">> sendRequest()");
-        serverHandler.sendData(sendingData);
+        let result = await serverHandler.sendData(sendingData, flag_wait_for_answer, callback);
+        if (result !== undefined && result !== null && result.constructor == Object && "error" in result) {
+            alert("Отсутствует соединение с сервером!");
+            FLAG_END = true;
+        }
         console.log("<< sendRequest()");
     }
 </script>
--- js/ServerHandler.js
+++ js/ServerHandler.js
@@ -68,8 +68,9 @@ export default class ServerHandler {
     ////
 
     async connect() {
+        console.log("Connecting to server...")
         this.socket = new WebSocket(this.url);
-        this.defineInitOnMessage();
+        this.initDefaultOnFunctions();
     }
 
     async reconnectIfNotConnected() {
@@ -88,33 +89,42 @@ export default class ServerHandler {
     }
 
     async waitForServerData() {
-        let timeout = 5;
+        let timeout = 10;
+        let step = 0.5;
         let count_time = 0;
         while (this.serverData == null && count_time < timeout) {
             // console.log("sleepping...")
-            await sleep(0.5);
-            count_time += 0.5;
+            await sleep(step);
+            count_time += step;
         }
         if (this.serverData == null)
             return { "error": "Данные с сервера не получены!" }
         return this.serverData;
     }
 
-    async sendData(data, flag_wait_for_answer = false) {
+    async sendData(data, flag_wait_for_answer = false, callback = null) {
 
         // console.log("sendData()");
 
-        let context = this;
+        if (!this.isEnabled()) {
+            return { "error": "Отсутствует соединение с сервером!" };
+        }
+
+        var context = this;
         this.waitForConnection(function () {
             context.serverData = null;
             context.socket.send(JSON.stringify(Array.from(data.entries())));
-            if (typeof callback !== 'undefined') {
-                callback();
-            }
+            // if (typeof callback !== 'undefined') {
+            //     callback();
+            // }
         }, 1000);
 
-        if (flag_wait_for_answer) {
-            return await this.waitForServerData();
+        if (flag_wait_for_answer && callback != null) {
+            // this.setCollbackOnMessage(callback);
+            let server_data = await this.waitForServerData();
+            if ("error" in server_data)
+                return server_data;
+            callback(server_data);
         }
 
         return "Запрос отправлен!"
@@ -140,28 +150,161 @@ export default class ServerHandler {
     //// JSON GETTERS
     ////
 
-    getTestSearchRequestData() {
+    getDonaldsonP55077SearchRequestData() {
         return new Map([
             ['flag', "SearchRequests"],
             [
                 'requests', [
-                    ["DONALDSON", "P550777"],
-                    ["MANN", "P550777"]
+                    ["DONALDSON", "P55077"]
                 ]
             ]
         ]);
     }
 
-    getSearchProgressRequestData() {
+    getFleetguardP55077SearchRequestData() {
+        return new Map([
+            ['flag', "SearchRequests"],
+            [
+                'requests', [
+                    ["FLEETGUARD", "P55077"]
+                ]
+            ]
+        ]);
+    }
+
+    getMannP55077SearchRequestData() {
+        return new Map([
+            ['flag', "SearchRequests"],
+            [
+                'requests', [
+                    ["MANN", "P55077"]
+                ]
+            ]
+        ]);
+    }
+
+    getHifiSh60161SearchRequestData() {
+        return new Map([
+            ['flag', "SearchRequests"],
+            [
+                'requests', [
+                    ["HIFI", "SH60161"]
+                ]
+            ]
+        ]);
+    }
+
+    getFilfilterP55077SearchRequestData() {
+        return new Map([
+            ['flag', "SearchRequests"],
+            [
+                'requests', [
+                    ["FILFILTER", "P55077"]
+                ]
+            ]
+        ]);
+    }
+
+    // 
+    // 
+    // 
+
+    getFastCheckSearchRequestData() {
+        return new Map([
+            ['flag', "SearchRequests"],
+            [
+                'requests', [
+                    ["DONALDSON", "P550777"],
+                    ["FLEETGUARD", "P550777"],
+                    ["MANN", "P550777"],
+                    ["HIFI", "SH60161"],
+                    ["FILFILTER", "P550777"],
+                ]
+            ]
+        ]);
+    }
+
+    getStandartCheckSearchRequestData() {
+        return new Map([
+            ['flag', "SearchRequests"],
+            [
+                'requests', [
+                    ["DONALDSON", "P55077"],
+                    ["FLEETGUARD", "P55077"],
+                    ["MANN", "P55077"],
+                    ["HIFI", "SH60161"],
+                    ["FILFILTER", "P55077"],
+                ]
+            ]
+        ]);
+    }
+
+
+
+    getDeepCheckTestSearchRequestData() {
+        return new Map([
+            ['flag', "SearchRequests"],
+            [
+                'requests', [
+                    ["FLEETGUARD", "FS19572"],
+                    ["FLEETGUARD", "P111"],
+                    ["FLEETGUARD", "P5507"],
+                    ["FLEETGUARD", "P55077765568"],
+                    ["MANN", "C10050"],
+                    ["MANN", "FP24"],
+                    ["MANN", "W14"],
+                    ["MANN", "FP24"],
+                    ["MANN", "P55077"],
+                    ["MANN", "H1"],
+                    ["HIFI", "SH60161"],
+                    ["HIFI", "SH6016"],
+                    ["HIFI", "P550777"],
+                    ["HIFI", "P5517"],
+                    ["FILFILTER", "ZP32890172531526"],
+                    ["FILFILTER", "ZP69"],
+                    ["FILFILTER", "P550777"],
+                    ["FILFILTER", "ZP507"],
+                    ["DONALDSON", "P5507713124"],
+                    ["DONALDSON", "P550777"],
+                    ["DONALDSON", "P550771"],
+                    ["DONALDSON", "ZP11"],
+                    ["DONALDSON", "P55077"]
+                ]
+            ]
+        ]);
+    }
+
+    // 
+    // 
+    // 
+
+    getGetLogRequestData() {
+        return new Map([
+            ['flag', "GetSearchLog"]
+        ]);
+    }
+
+    getGetProgressRequestData() {
         return new Map([
             ['flag', "GetSearchProgress"]
         ]);
     }
 
-    getSearchRequestData(search_requests) {
+    getGetResultRequestData() {
         return new Map([
-            ['flag', "SearchRequests"],
-            ['requests', search_requests]
+            ['flag', "GetSearchResult"]
+        ]);
+    }
+
+    getGetLogProgressResultRequestData() {
+        return new Map([
+            ['flag', "GetSearchLogProgressResult"]
+        ]);
+    }
+
+    getGetSearchFlagEndRequestData() {
+        return new Map([
+            ['flag', "GetSearchFlagEnd"]
         ]);
     }
 
@@ -180,22 +323,83 @@ export default class ServerHandler {
         return JSON.parse(data);
     }
 
-    defineInitOnMessage() {
+    initDefaultOnFunctions() {
+        var context = this;
+
+        context.socket.onopen = function (event) {
+            console.log("The connection was opened!");
+        };
+
+        this.initDefaultOnMessage();
+
+        context.socket.onerror = function (event) {
+            console.log("Websocket error!");
+        }
+
+        context.socket.onclose = function (event) {
+            var reason;
+            if (event.code == 1000)
+                reason = "Normal closure, meaning that the purpose for which the connection was established has been fulfilled.";
+            else if (event.code == 1001)
+                reason = "An endpoint is \"going away\", such as a server going down or a browser having navigated away from a page.";
+            else if (event.code == 1002)
+                reason = "An endpoint is terminating the connection due to a protocol error";
+            else if (event.code == 1003)
+                reason = "An endpoint is terminating the connection because it has received a type of data it cannot accept (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).";
+            else if (event.code == 1004)
+                reason = "Reserved. The specific meaning might be defined in the future.";
+            else if (event.code == 1005)
+                reason = "No status code was actually present.";
+            else if (event.code == 1006)
+                reason = "The connection was closed abnormally, e.g., without sending or receiving a Close control frame";
+            else if (event.code == 1007)
+                reason = "An endpoint is terminating the connection because it has received data within a message that was not consistent with the type of the message (e.g., non-UTF-8 [https://www.rfc-editor.org/rfc/rfc3629] data within a text message).";
+            else if (event.code == 1008)
+                reason = "An endpoint is terminating the connection because it has received a message that \"violates its policy\". This reason is given either if there is no other sutible reason, or if there is a need to hide specific details about the policy.";
+            else if (event.code == 1009)
+                reason = "An endpoint is terminating the connection because it has received a message that is too big for it to process.";
+            else if (event.code == 1010) // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
+                reason = "An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake. <br /> Specifically, the extensions that are needed are: " + event.reason;
+            else if (event.code == 1011)
+                reason = "A server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.";
+            else if (event.code == 1015)
+                reason = "The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).";
+            else
+                reason = "Unknown reason";
+
+            console.log(event.code + " The connection was closed for reason: " + reason);
+            alert(event.code + " The connection was closed for reason: " + reason);
+        };
+    }
+
+    initDefaultOnMessage() {
         var context = this;
 
         context.socket.onmessage = (event) => {
             try {
                 context.serverData = context.parseServerData(event.data);
+                console.log("serverData:", context.serverData)
             }
             catch (error) {
                 console.log(error);
             }
         };
+    }
 
-        context.socket.onerror = function (event) {
-            console.log("onerror(): " + event);
-        }
+    setCollbackOnMessage(callback) {
+        var context = this;
 
+        context.socket.onmessage = (event) => {
+            try {
+                context.serverData = context.parseServerData(event.data);
+                console.log("serverData:", context.serverData);
+                callback(context.serverData);
+                this.initDefaultOnMessage();
+            }
+            catch (error) {
+                console.log(error);
+            }
+        };
     }
 
     waitForConnection(callback, interval) {
